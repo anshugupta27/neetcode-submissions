2c46175c4efc456d8625117d1f2eefe9f64ded2a
Anshu Gupta <57598091+anshugupta27@users.noreply.github.com>
2026-04-07 12:58:08 -0400

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void permuteUtil (vector<int>& nums, int index, vector<vector<int>> &finalAns) {
+        if (index >= nums.size()) {
+            finalAns.push_back (nums);
+            return;
+        }
+        for (int i = index ; i < nums.size() ; i++) {
+            swap (nums[i], nums[index]);
+            permuteUtil (nums, index+1, finalAns);
+            swap(nums[i], nums[index]);
+        }
+
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> finalAns;
+        permuteUtil (nums, 0, finalAns);
+        return finalAns;
+        
+    }
+};
